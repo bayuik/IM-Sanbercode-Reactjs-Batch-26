c1aaab997d7a170814820f04d7151c926f5c80b4
bayuik <bayuindrakusuma05.com>
2021-07-30 05:28:26 +0700

    add tugas14

--- Tugas-Harian-Part-2/src/App.js
+++ Tugas-Harian-Part-2/src/App.js
@@ -2,7 +2,8 @@
 // import Tugas10 from './Tugas-10/Tugas10';
 // import Tugas11 from './Tugas-11/Tugas11';
 // import Tugas12 from './Tugas-12/Tugas12';
-import Tugas13 from './Tugas-13/Tugas13';
+// import Tugas13 from './Tugas-13/Tugas13';
+import Student from './Tugas-14/Student';
 
 function App() {
     return (
@@ -11,7 +12,8 @@ function App() {
             <Tugas10 />
             <Tugas11 />
             <Tugas12 /> */}
-            <Tugas13 />
+            {/* <Tugas13 /> */}
+            <Student />
         </>
     );
 }
